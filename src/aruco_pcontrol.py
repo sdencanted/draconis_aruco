f74c17932f81659816f92f900a486940f79e21cb
sdencanted <peeps.in.microwaves.explode@gmail.com>
2023-10-19 12:10:42 +0800

    use fiducials

--- src/aruco_pcontrol.py
+++ src/aruco_pcontrol.py
@@ -1,31 +1,35 @@
 #!/usr/bin/env python3
 import rospy
-from geometry_msgs.msg import PoseStamped
 import tf2_ros
 import tf_conversions
-import tf2_geometry_msgs
+from fiducial_msgs.msg import FiducialTransform,FiducialTransformArray
 from mavros_msgs.msg import State, PositionTarget
 rospy.init_node('aruco_pcontrol', anonymous=True)
-
+import numpy as np
 target_pub = rospy.Publisher('target', PositionTarget, queue_size=10)
 x_p_gain = float(rospy.get_param('~x_p_gain'))
 x_pos_goal = float(rospy.get_param('~x_pos_goal'))
 y_p_gain = float(rospy.get_param('~y_p_gain'))
+yaw_vy_p_gain = float(rospy.get_param('~yaw_vy_p_gain'))
 yaw_p_gain = float(rospy.get_param('~yaw_p_gain'))
 target_frame=rospy.get_param('~drone_frame')
+max_v=rospy.get_param('~max_v')
+max_yaw_rate=rospy.get_param('~max_yaw_rate')
 
 tf_buffer = tf2_ros.Buffer(rospy.Duration(100.0))  # tf buffer length
 tf_listener = tf2_ros.TransformListener(tf_buffer)
 transform = None
+mavros_state_flying=False
 
+#Positive yaw  rate is clockwise
 def constructTarget(vx, vy, yaw_rate):
     target_raw_pose = PositionTarget()
     target_raw_pose.header.stamp = rospy.Time.now()
 
     target_raw_pose.coordinate_frame = PositionTarget.FRAME_BODY_NED  # 8
-    target_raw_pose.velocity.x = vx
-    target_raw_pose.velocity.y = vy
-    target_raw_pose.yaw_rate = yaw_rate
+    target_raw_pose.velocity.x = np.clip(vx,-max_v,max_v)
+    target_raw_pose.velocity.y = np.clip(vy,-max_v,max_v)
+    target_raw_pose.yaw_rate = np.clip(yaw_rate,-max_yaw_rate,max_yaw_rate)
 
     mask = PositionTarget.IGNORE_PX | PositionTarget.IGNORE_PY | PositionTarget.IGNORE_PZ \
         | PositionTarget.IGNORE_VZ \
@@ -36,38 +40,30 @@ def constructTarget(vx, vy, yaw_rate):
 
     return target_raw_pose
 
+def fiducialCb(data: FiducialTransformArray):
+    
+    for fid in data.transforms:
+        if fid.fiducial_id!=5:
+            continue
+        rospy.loginfo("got an aruco transform")
+        
+        x_error=x_pos_goal-fid.transform.translation.z
+        y_error=-(-fid.transform.translation.x)
+        quat=fid.transform.rotation
+        yaw_error = tf_conversions.transformations.euler_from_quaternion(
+                    [quat.x, quat.y, quat.z, quat.w])[2]
+        # rospy.loginfo(f"{x_error},{y_error},{yaw_error}")
+        rospy.loginfo(f"{tf_conversions.transformations.euler_from_quaternion([quat.x, quat.y, quat.z, quat.w])[0]},{tf_conversions.transformations.euler_from_quaternion([quat.x, quat.y, quat.z, quat.w])[1]},{tf_conversions.transformations.euler_from_quaternion([quat.x, quat.y, quat.z, quat.w])[2]}")
+        if mavros_state_flying:
+            vx=x_error*x_p_gain
+            vy=y_error*y_p_gain+yaw_error*yaw_vy_p_gain*fid.transform.translation.z
 
-def arucoCb(data: PoseStamped):
-    rospy.loginfo("got an aruco pose")
-    if transform is not None and transform!=-1:
-        data = tf2_geometry_msgs.do_transform_pose(data, transform)
-    elif target_frame!='':
+            # pos yaw rate is clockwise
+            vyaw=yaw_error*yaw_p_gain
+            target=constructTarget(vx,vy,vyaw)
+            target_pub.publish(target)
 
-        try:
-            transform = tf_buffer.lookup_transform(target_frame,
-                                        # source frame:
-                                        data.header.frame_id,
-                                        # get the tf at the time the pose was valid
-                                        data.header.stamp,
-                                        # wait for at most 1 second for transform, otherwise throw
-                                        rospy.Duration(1.0))
-
-        except:
-            rospy.warn("could not get transformed pose, using pose from camera as is")
-            transform=-1
-    x_error=x_pos_goal-data.pose.position.x
-    y_error=-data.pose.position.y
-    quat=data.pose.orientation
-    yaw_error = -tf_conversions.transformations.euler_from_quaternion(
-                [quat.x, quat.y, quat.z, quat.w])[2]
-    if mavros_state_flying:
-        vx=x_error*x_p_gain
-        vy=y_error*y_p_gain
-        vyaw=yaw_error*yaw_p_gain
-        target=constructTarget(vx,vy,vyaw)
-        target_pub.publish(target)
-
-        # change to pos x(default 1m) and vy, do not touch vz
+            # change to pos x(default 1m) and vy, do not touch vz
 
 
 def mavrosCb(data: State):
@@ -80,8 +76,10 @@ def mavrosCb(data: State):
 
 
 def startPControl():
-    rospy.Subscriber("/aruco_single/pose",
-                     PoseStamped, arucoCb, queue_size=1)
+    # rospy.Subscriber("/aruco_single/pose",
+    #                  PoseStamped, arucoCb, queue_size=1)
+    rospy.Subscriber("/fiducial_transforms",
+                     FiducialTransformArray, fiducialCb, queue_size=1)
     rospy.Subscriber("/mavros/state", State, mavrosCb, queue_size=1)
     rospy.spin()
 
